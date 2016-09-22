650cf7f216f506a3cf996b7e94571e4d63674cdb
fayong <eniac314@gmail.com>
2016-09-22 18:51:53 +0800

    hopefully better tokenizer

--- src/BetterParser.elm
+++ src/BetterParser.elm
@@ -5,6 +5,7 @@ import Html.Attributes exposing (..)
 import List exposing (member, foldr, reverse, head)
 import String exposing (cons, words, endsWith, startsWith, dropLeft, dropRight, uncons, isEmpty, fromChar)
 import Dict exposing (fromList, get)
+import Tokenizer exposing (..)
 
 type alias Res b a = Result String (b,a)
 type Parser a b = Parser (a -> Res b a)
@@ -254,9 +255,10 @@ parseTagList = token "["
                 >>* many parseTag [] (::)
                 >>= (\res -> token "]" >>* return res)
 
-res = (tokenizer testinput4)
-res2 = parse parseAttrList (words testinput3)
-res3 = parse  parseStyle (words testinput4)
+res = (tokenizer' ( tagPos testinput))
+res2 = tokenizer testinput4
+res3 = parse parseAttrList (words testinput3)
+res4 = parse  parseStyle (words testinput4)
 
 testinput = 
   """ div [ class "mainDiv" ]
@@ -284,117 +286,3 @@ testinput4 =
   
   """
 
---------------------------------------------------------------------------------
---TOKENIZER
-
-type alias Token = 
-  { val  : String
-  , ln   : (Int, String)
-  }
-
-tokError : Token -> String
-tokError t = "{ val: " ++ (.val t) 
-             ++ ", line " ++ (toString (fst (.ln t)))
-             ++ ", " ++ (toString (snd (.ln t))) ++ " }"
-
-tokenizer : String -> Result String (List String)
-tokenizer s = (words s) |> splitter 
-
-isTokenChar : Char-> Bool
-isTokenChar c = 
-  c == '(' ||
-  c == ')' ||
-  c == '{' ||
-  c == '}' ||
-  c == '+' ||
-  c == '-' ||
-  c == '/' ||
-  c == '*' ||
-  c == ';' ||
-  c == ',' ||
-  c == '.' ||
-  c == '[' ||
-  c == ']' ||
-  c == '&' ||
-  c == '|' ||
-  c == '>' ||
-  c == '<' ||
-  c == '=' ||
-  c == '~'  
-
-
-splitter : List String -> Result String (List String)
-splitter xs = 
-  case xs of 
-    []      -> Ok []
-    (y::ys) ->
-      if startsWith "\"" y
-      then case getStringLiteral xs of 
-            Err s       -> Err s
-            Ok (sl,out) -> 
-              let rest = splitter out
-              in  case rest of 
-                Err s   -> Err s
-                Ok  r   -> Ok (sl::r)
-      else case getTokens y of 
-            Err s       -> Err s
-            Ok ts       -> 
-              let rest = splitter ys
-              in  case rest of 
-                Err s   -> Err s
-                Ok  r   -> Ok (ts ++ r)
-
-getStringLiteral : List String -> Result String (String, List String) 
-getStringLiteral xs = 
-  case xs of 
-    
-    [] -> Err "Invalid string literal"
-
-    (s :: []) -> 
-      case (splitOn (\c -> c == '\"') s) of 
-        (a::b::xs) -> Ok (a ++ b, xs)
-        --(a::xs) -> Ok (a, xs)
-        _ -> Err "Invalid string literal" 
-    
-    (s::ss) -> 
-      case (splitOn (\c -> c == '\"') s) of 
-        (a::b::xs) -> Ok (a ++ b, xs ++ ss)
-        _ -> case getStringLiteral ss of
-               Err s -> Err s
-               Ok (end,rest) -> Ok (s ++ " " ++ end, rest)
-      --if endsWith "\"" s
-      --then Ok (s,[])
-      --else Err "Invalid string literal"
-    
-    --(s::ss) -> 
-    --  if endsWith "\"" s
-    --  then Ok (s,ss)
-    --  else case getStringLiteral ss of
-    --         Err s -> Err s
-    --         Ok (end,rest) -> Ok (s ++ " " ++ end, rest)
-
-getTokens : String -> Result String (List String)
-getTokens xs =
-  Ok (splitOn isTokenChar xs)
-
-splitOn :  (Char -> Bool) -> String -> List String
-splitOn p s = 
-  let 
-  helper (inp,out,buff) = 
-      case uncons inp of
-        Nothing -> (inp, addBuffer buff out, buff)
-        Just (c,ss) ->
-          if p c 
-          then helper (ss, (fromChar c) :: (addBuffer buff out), "")
-          else helper (ss,out,cons c buff)
-  
-  addBuffer buff out = 
-    if isEmpty buff 
-    then out
-    else (String.reverse buff) :: out 
-
-  (_,res,_) = helper (s,[],"")
-
-  in List.reverse res 
-
-  
---
+++ src/Tokenizer.elm
@@ -0,0 +1,270 @@
+module Tokenizer exposing (..)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import List exposing (member, foldr, foldl, reverse, head)
+import String exposing (cons
+                       , words
+                       , lines
+                       , endsWith
+                       , startsWith
+                       , dropLeft
+                       , dropRight
+                       , uncons
+                       , isEmpty
+                       , fromChar
+                       , toList)
+
+import Dict exposing (fromList, get)
+
+--------------------------------------------------------------------------------
+--TOKENIZER
+
+type alias Token = 
+  { val  : String
+  , ch   : Int
+  , ln   : Int
+  }
+
+tokError : Token -> String
+tokError t = "{ val: " ++ (.val t) 
+             ++ ", char " ++ (toString (.ch t))
+             ++ ", line " ++ (toString (.ln t))
+             ++ " }"
+
+tokenizer : String -> Result String (List String)
+tokenizer s = (words s) |> splitter 
+
+isSpace : Char-> Bool
+isSpace c = 
+  c == '\r' ||
+  c == ' ' ||
+  c == '\t' ||
+  c == '\n'
+
+isTokenChar : Char-> Bool
+isTokenChar c = 
+  c == '(' ||
+  c == ')' ||
+  c == '{' ||
+  c == '}' ||
+  c == '+' ||
+  c == '-' ||
+  c == '/' ||
+  c == '*' ||
+  c == ';' ||
+  c == ',' ||
+  c == '.' ||
+  c == '[' ||
+  c == ']' ||
+  c == '&' ||
+  c == '|' ||
+  c == '>' ||
+  c == '<' ||
+  c == '=' ||
+  c == '~'  
+
+
+splitter : List String -> Result String (List String)
+splitter xs = 
+  case xs of 
+    []      -> Ok []
+    (y::ys) ->
+      if startsWith "\"" y
+      then case getStringLiteral xs of 
+            Err s       -> Err s
+            Ok (sl,out) -> 
+              let rest = splitter out
+              in  case rest of 
+                Err s   -> Err s
+                Ok  r   -> Ok (sl::r)
+      else case getTokens y of 
+            Err s       -> Err s
+            Ok ts       -> 
+              let rest = splitter ys
+              in  case rest of 
+                Err s   -> Err s
+                Ok  r   -> Ok (ts ++ r)
+
+getStringLiteral : List String -> Result String (String, List String) 
+getStringLiteral xs = 
+  case xs of 
+    
+    [] -> Err "Invalid string literal"
+
+    (s :: []) -> 
+      case (splitOn (\c -> c == '\"') s) of 
+        (a::b::xs) -> Ok (a ++ b, xs)
+        --(a::xs) -> Ok (a, xs)
+        _ -> Err "Invalid string literal" 
+    
+    (s::ss) -> 
+      case (splitOn (\c -> c == '\"') s) of 
+        (a::b::xs) -> Ok (a ++ b, xs ++ ss)
+        _ -> case getStringLiteral ss of
+               Err s -> Err s
+               Ok (end,rest) -> Ok (s ++ " " ++ end, rest)
+      --if endsWith "\"" s
+      --then Ok (s,[])
+      --else Err "Invalid string literal"
+    
+    --(s::ss) -> 
+    --  if endsWith "\"" s
+    --  then Ok (s,ss)
+    --  else case getStringLiteral ss of
+    --         Err s -> Err s
+    --         Ok (end,rest) -> Ok (s ++ " " ++ end, rest)
+
+getTokens : String -> Result String (List String)
+getTokens xs =
+  Ok (splitOn isTokenChar xs)
+
+splitOn :  (Char -> Bool) -> String -> List String
+splitOn p s = 
+  let 
+  helper (inp,out,buff) = 
+      case uncons inp of
+        Nothing -> (inp, addBuffer buff out, buff)
+        Just (c,ss) ->
+          if p c 
+          then helper (ss, (fromChar c) :: (addBuffer buff out), "")
+          else helper (ss,out,cons c buff)
+  
+  addBuffer buff out = 
+    if isEmpty buff 
+    then out
+    else (String.reverse buff) :: out 
+
+  (_,res,_) = helper (s,[],"")
+
+  in List.reverse res 
+
+  -----------------------------------------------------------------------------
+
+type alias CharPos = 
+  { ch  : Char
+  , lnp : Int
+  , chp : Int
+  }
+
+type alias CharMeta = List CharPos
+
+tagPos : String -> CharMeta
+tagPos s = 
+  let ls = addIndexes (lines s)
+      cs = List.map
+            (\(n,l) -> toList l  
+                       |> addIndexes
+                       |> List.map 
+                           (\(m,c) -> CharPos c n m)) ls 
+  in List.concat cs 
+
+
+getStringLit : CharMeta -> Result String (CharMeta,CharMeta)
+getStringLit xs = 
+  case xs of 
+    [] -> Err "getStringLit: Invalid String literal"
+    (x::xs) -> 
+      if (.ch x == '\"')
+      then Ok ([],xs)
+      else 
+        case getStringLit xs of 
+          Err s -> Err s
+          Ok (l,r) -> Ok (x::l, r) 
+
+
+tokenString : Result String (CharMeta, CharMeta) -> Result String (Token,CharMeta)
+tokenString xs = 
+  case xs of 
+    Err s -> Err s
+    Ok ([],_) -> Err "tokenString: Invalid String literal" 
+    Ok ((c::cs),rest) ->
+      let ch = .chp c
+          ln = .lnp c
+          val = foldr (\c s -> cons (.ch c) s) "" (c::cs)
+      in Ok ((Token ("\"" ++ val ++ "\"") ch ln),rest)
+
+
+getTokens' : CharMeta -> (List Token,CharMeta)
+getTokens' xs = 
+  let tokenize buff = 
+        let ys = List.reverse buff
+        in case ys of 
+             [] -> []
+             (y::ys) -> 
+             [Token (tString (y::ys)) (.chp y) (.lnp y)]
+
+      tString xs = String.fromList (List.map (.ch) xs) 
+
+      helper xs acc = 
+        case xs of 
+        [] -> (tokenize acc,[])
+        (x::xs) -> 
+         if (isTokenChar (.ch x))
+         then 
+          (tokenize acc ++
+           [Token (fromChar (.ch x)) (.chp x) (.lnp x)]
+          , xs
+          )
+         else if (isSpace (.ch x))
+              then (tokenize acc, xs)
+              else if (.ch x == '\"')
+                   then (tokenize acc, x::xs)
+                   else helper xs (x::acc)
+  in helper xs [] 
+  
+
+
+
+
+tokenizer' : CharMeta -> Result String (List Token)
+tokenizer' cs = 
+  case cs of 
+    [] -> Ok []
+    (x::xs) -> 
+      if (.ch x == '\"')
+      then 
+        case tokenString (getStringLit xs) of
+          Err s -> Err s 
+          Ok (t,rest) -> 
+            case tokenizer' rest of
+              Err s  -> Err s
+              Ok ts' -> Ok (t :: ts')
+      
+      else 
+        if isSpace (.ch x)
+        then tokenizer' xs
+        
+        else 
+          let (ts,rest) = getTokens' (x::xs)
+          in case tokenizer' rest of
+              Err s  -> Err s
+              Ok ts' -> Ok (ts ++ ts')   
+          
+          
+
+-------------------------------------------------------------------------------
+
+addIndexes : List a -> List (Int , a)
+addIndexes xs = 
+  let helper n xs = 
+    case xs of
+     [] -> []
+     (x::xs) -> (n,x) :: helper (n+1) xs
+  in helper 0 xs
+
+removeIndexes : List (Int, a) -> List a
+removeIndexes xs = List.map snd xs
+
+
+break : (a -> Bool) -> List a -> (List a, List a)
+break p xs = 
+  let 
+    helper ys left = 
+      case ys of 
+        [] -> (left,[])
+        (y::ys) -> 
+          if p y 
+          then (List.reverse (y :: left), ys)
+          else helper ys (y :: left)
+  in helper xs []
